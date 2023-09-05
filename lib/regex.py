9b543da81276a7074aa79bb4efba9afb7783863f
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-05 13:23:03 +0300

    Done

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,21 @@
-import re
-
-my_pattern = r""
-my_regex = re.compile(my_pattern)
-
+[{
+	"resource": "/home/stephan/python-p3-regex-bonus-lab/lib/testing/regex_test.py",
+	"owner": "_generated_diagnostic_collection_name_#6",
+	"code": {
+		"value": "reportMissingModuleSource",
+		"target": {
+			"$mid": 1,
+			"path": "/microsoft/pyright/blob/main/docs/configuration.md",
+			"scheme": "https",
+			"authority": "github.com",
+			"fragment": "reportMissingModuleSource"
+		}
+	},
+	"severity": 4,
+	"message": "Import \"regex\" could not be resolved from source",
+	"source": "Pylance",
+	"startLineNumber": 4,
+	"startColumn": 6,
+	"endLineNumber": 4,
+	"endColumn": 11
+}]
